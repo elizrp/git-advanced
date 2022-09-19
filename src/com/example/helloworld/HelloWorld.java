93bbefe9fbd9885c0c18031d2275f84a3bf5eef2
Elizabeth Pavlova <elizabet.pavlova@estafet.com>
2022-09-19 10:01:20 +0300

    Modify hello message from master branch

--- src/com/example/helloworld/HelloWorld.java
+++ src/com/example/helloworld/HelloWorld.java
@@ -2,6 +2,7 @@ package com.example.helloworld;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello world! Let's create a merge conflict!");
+        System.out.println("Hello world! Let's create a merge conflict! " +
+                "Master branch.");
     }
 }
